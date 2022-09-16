88ce4184dea37a0dba023f00f949435f1f19c480
sivasankar10 <111382075+sivasankar10@users.noreply.github.com>
2022-09-16 08:56:42 +0530

    Create swap without temp .c

---
+++ swap without temp .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b;
+	printf("a:");
+	scanf("%d",&a);
+	printf("b:");
+	scanf("%d",&b);
+	a =a*b; 
+    b =a/b; 
+    a=a/b;
+    printf("a:%d b:%d",a,b);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
